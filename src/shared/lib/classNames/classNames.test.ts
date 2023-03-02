0ead86c3063f45d73f8f677e5d860ed00b08100f
Liam <backgroundpositioncenter@gmail.com>
2023-03-03 01:11:25 +0300

    added jest, added first test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,12 @@
+import {classNames} from "./classNames";
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+
+    test('with additional class', () => {
+        const expected = 'someClass class1 class2'
+        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
+    });
+});
